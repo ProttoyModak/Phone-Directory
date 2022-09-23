e36ace35f71b1d3d630532e281a6a3e743c93088
Prottoy Modak <90288176+ProttoyModak@users.noreply.github.com>
2022-09-23 23:04:33 +0600

    Looping Process

--- phoneDirectory.sh
+++ phoneDirectory.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
-# Adding Options to the Landing Page
-echo "--------------------------"
-echo "Welcome to Phone Directory"
-echo "--------------------------"
-echo ""
-echo "1. New Contact"
-echo "2. Delete Contact"
-echo "3. Search Contact"
-echo "4. View Directory"
-echo "5. Exit"
+# Creating Loop To Work Multiple Times
+while :
+do
+    # Adding Options to the Landing Page
+    echo "--------------------------"
+    echo "Welcome to Phone Directory"
+    echo "--------------------------"
+    echo ""
+    echo "1. New Contact"
+    echo "2. Delete Contact"
+    echo "3. Search Contact"
+    echo "4. View Directory"
+    echo "5. Exit"
 
-# Taking User Input For Selecting Option
-echo ""
-read -p "Enter your choise: " user
-clear
+    # Taking User Input For Selecting Option
+    echo ""
+    read -p "Enter your choise: " user
+    clear
 
-# Creating Test Case For Options
-case $user in
+    # Creating Test Case For Options
+    case $user in
 
-1)
+        1)
 
-;;
+        ;;
 
-2)
+        2)
 
-;;
+        ;;
 
-3)
+        3)
 
-;;
+        ;;
 
-4)
+        4)
 
-;;
+        ;;
 
-5)
-break
-;;
+        5)
+        break
+        ;;
 
-*)
-echo "INVALID OPARATION"
-;;
-esac
+        *)
+        echo "INVALID OPARATION"
+        ;;
+    esac
 
-# Creating Exit Option
-read -p "Press Any Key For Continue or Press 5 To Exit: " exit
+    # Creating Exit Option
+    read -p "Press Any Key For Continue or Press 5 To Exit: " exit
 
-if [ $exit -eq 5] ; then
-break
-fi
+    if [ $exit -eq 5] ; then
+        break
+    fi
+done
