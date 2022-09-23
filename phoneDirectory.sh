2c358b075f97ec1b246dd7d89efab69810e343cb
Prottoy Modak <90288176+ProttoyModak@users.noreply.github.com>
2022-09-23 22:46:08 +0600

    Landing page options

---
+++ phoneDirectory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Designing Landing Page
+echo "--------------------------"
+echo "Welcome to Phone Directory"
+echo "--------------------------"
+echo ""
+echo "1. New Contact"
+echo "2. Delete Contact"
+echo "3. Search Contact"
+echo "4. View Directory"
+echo "5. Exit"
